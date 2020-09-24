a4d9cff8b0384ff2c790a40a24e35a7542641502
Leo <leozhang@cmbchina.com>
2020-09-24 14:05:20 +0800

    update

--- custom/custom-cn.el
+++ custom/custom-cn.el
@@ -1,10 +1,10 @@
 ;;; custom-cn.el --- chinese environment -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-
 (maybe-require-package 'rime)
 
 (setq rime-show-candidate 'popup)
+(setq default-input-method 'rime)
 
 (provide 'custom-cn)
 ;;; custom-cn.el ends here
