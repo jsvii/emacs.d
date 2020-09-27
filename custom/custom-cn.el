27ea97b20f7113dc614c249893c853ea94e73b23
Leo <leozhang@cmbchina.com>
2020-09-27 17:26:20 +0800

    update

--- custom/custom-cn.el
+++ custom/custom-cn.el
@@ -1,10 +1,12 @@
 ;;; custom-cn.el --- chinese environment -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-(maybe-require-package 'rime)
 
-(setq rime-show-candidate 'popup)
-(setq default-input-method 'rime)
+;; WSL: WSL1 has "-Microsoft", WSL2 has "-microsoft-standard"
+(when (not (string-match "-[Mm]icrosoft" operating-system-release))
+  (maybe-require-package 'rime)
+  (setq rime-show-candidate 'popup)
+  (setq default-input-method 'rime))
 
 (provide 'custom-cn)
 ;;; custom-cn.el ends here
