5fa5b11c50078dc29b450253f5fd71922decf056
Leo <leozhang@cmbchina.com>
2020-10-13 10:47:28 +0800

    update

--- custom/custom-cn.el
+++ custom/custom-cn.el
@@ -1,14 +1,15 @@
 ;;; custom-cn.el --- chinese environment -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-
+;; not WSL, ssh client, macos
 ;; WSL: WSL1 has "-Microsoft", WSL2 has "-microsoft-standard"
-(when (and
-       (not (string-match "-[Mm]icrosoft" operating-system-release))
-       (not (eq system-type 'darwin)))
-  (maybe-require-package 'rime)
-  (setq rime-show-candidate 'popup)
-  (setq default-input-method 'rime))
+(when (and (not (getenv "SSH_CLIENT"))
+           (not (string-match "-[Mm]icrosoft" operating-system-release))
+           (not (eq system-type 'darwin)))
+  (when (maybe-require-package 'rime)
+    (setq rime-show-candidate 'popup)
+    (setq default-input-method 'rime)
+    ))
 
 (provide 'custom-cn)
 ;;; custom-cn.el ends here
--- custom/custom-lsp.el
+++ custom/custom-lsp.el
@@ -1,20 +1,38 @@
 ;;; custom-lsp.el --- all lsp mode for development -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-
 (when (maybe-require-package 'web-mode)
   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (maybe-require-package 'flycheck)
   (with-eval-after-load 'flycheck
     (flycheck-add-mode 'typescript-tslint 'web-mode)
-    )
-  )
-
-
+    ))
 ;;;
+;;;;;;;; javascript & typescript
 ;;; npm i -g javascript-typescript-langserver
 ;;; npm i -g typescript-language-server; npm i -g typescript
+;;;;;;;; css
+;;; npm install -g vscode-css-languageserver-bin
 ;;;
 (when (maybe-require-package 'lsp-mode)
+  ;; lsp-ui
+  (maybe-require-package 'lsp-ui)
+  (maybe-require-package 'lsp-treemacs)
+  )
+
+(with-eval-after-load 'ivy
+  (maybe-require-package 'lsp-ivy))
+
+(with-eval-after-load 'company
+  (maybe-require-package 'company-lsp))
+
+(with-eval-after-load 'lsp-treemacs
+  (setq lsp-treemacs-sync-mode t)
+  )
+
+;;  (add-to-list 'lsp-language-id-configuration '(js-jsx-mode . "javascriptreact"))
+;;(add-hook 'web-mode-hook #'lsp-flycheck-enable) ; enable flycheck-lsp for web-mode locally
+(with-eval-after-load 'lsp-mode
   ;; lsp configs
   (setq lsp-auto-guess-root t)
   (setq lsp-diagnostic-package :none)
@@ -29,58 +47,41 @@
   (setq lsp-enable-completion-at-point nil)
   (setq read-process-output-max (* 1024 1024)) ;; 1mb
   (setq lsp-idle-delay 0.5)
-  (setq lsp-prefer-capf t)
+  (setq lsp-prefer-capf t))
 
-  ;;  (add-to-list 'lsp-language-id-configuration '(js-jsx-mode . "javascriptreact"))
-  ;;(add-hook 'web-mode-hook #'lsp-flycheck-enable) ; enable flycheck-lsp for web-mode locally
-  (with-eval-after-load 'lsp-mode
-    (dolist (hook
-             '(c-mode-hook
-               c++-mode-hook
-               c-or-c++-mode-hook
-               java-mode-hook
-               js-mode-hook
-               js-jsx-mode-hook
-               js2-mode-hook
-               typescript-mode-hook
-               python-mode-hook
-               web-mode-hook))
-      (add-hook hook #'lsp))
-    ;;    (require 'lsp-javascript)
-    )
-
-  ;; lsp-ui
-  (when (maybe-require-package 'lsp-ui)
+(with-eval-after-load 'lsp-ui
+  (progn
+    (require 'lsp-ui-imenu)
     (setq lsp-ui-sideline-enable t)
     ;; disable showing docs on hover at the top of the window
     (setq lsp-ui-doc-enable nil)
     (setq lsp-ui-imenu-enable t)
     (setq lsp-ui-imenu-kind-position 'top)
-    (with-eval-after-load 'lsp-ui
-      (progn
-        (require 'lsp-ui-imenu)
-        (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-        (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-        (define-key lsp-ui-mode-map (kbd "C-c C-l .") 'lsp-ui-peek-find-definitions)
-        (define-key lsp-ui-mode-map (kbd "C-c C-l ?") 'lsp-ui-peek-find-references)
-        (define-key lsp-ui-mode-map (kbd "C-c C-l r") 'lsp-rename)
-        (define-key lsp-ui-mode-map (kbd "C-c C-l x") 'lsp-restart-workspace)
-        (define-key lsp-ui-mode-map (kbd "C-c C-l w") 'lsp-ui-peek-find-workspace-symbol)
-        (define-key lsp-ui-mode-map (kbd "C-c C-l i") 'lsp-ui-peek-find-implementation)
-        (define-key lsp-ui-mode-map (kbd "C-c C-l d") 'lsp-describe-thing-at-point)
-        (define-key lsp-ui-mode-map (kbd "C-c C-l e") 'lsp-execute-code-action)
-        )
-      )
-    )
-  (when (maybe-require-package 'lsp-treemacs)
-    (setq lsp-treemacs-sync-mode t))
+    (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+    (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+    (define-key lsp-ui-mode-map (kbd "C-c C-l .") 'lsp-ui-peek-find-definitions)
+    (define-key lsp-ui-mode-map (kbd "C-c C-l ?") 'lsp-ui-peek-find-references)
+    (define-key lsp-ui-mode-map (kbd "C-c C-l r") 'lsp-rename)
+    (define-key lsp-ui-mode-map (kbd "C-c C-l x") 'lsp-restart-workspace)
+    (define-key lsp-ui-mode-map (kbd "C-c C-l w") 'lsp-ui-peek-find-workspace-symbol)
+    (define-key lsp-ui-mode-map (kbd "C-c C-l i") 'lsp-ui-peek-find-implementation)
+    (define-key lsp-ui-mode-map (kbd "C-c C-l d") 'lsp-describe-thing-at-point)
+    (define-key lsp-ui-mode-map (kbd "C-c C-l e") 'lsp-execute-code-action)
+    ))
 
-  (with-eval-after-load 'ivy
-    (maybe-require-package 'lsp-ivy))
-
-  (with-eval-after-load 'company
-    (maybe-require-package 'company-lsp))
-  )
+(dolist (hook
+         '(c-mode-hook
+           c++-mode-hook
+           c-or-c++-mode-hook
+           java-mode-hook
+           js-mode-hook
+           js-jsx-mode-hook
+           js2-mode-hook
+           css-mode-hook
+           typescript-mode-hook
+           python-mode-hook
+           web-mode-hook))
+  (add-hook hook #'lsp))
 
 (provide 'custom-lsp)
 ;;; custom-lsp.el ends here
