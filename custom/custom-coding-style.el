cbddc45916e059b43d740529bb16b33b4b51254c
zhangguoliang <leozhang@cmbchina.com>
2021-02-04 02:56:22 +0800

    update term

--- custom/custom-coding-style.el
+++ custom/custom-coding-style.el
@@ -13,5 +13,8 @@
 (when (maybe-require-package 'undo-tree)
   (global-undo-tree-mode))
 
+(when (maybe-require-package 'editorconfig)
+  (editorconfig-mode 1))
+
 (provide 'custom-coding-style)
 ;;; custom-coding-style.el ends here
