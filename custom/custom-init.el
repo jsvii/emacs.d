ae54a396afd80e03987360e4bc286d1539afae1e
Leo Victor <zhangguoliang@zhaodongdb.com>
2021-11-18 10:52:51 +0800

    update

--- custom/custom-init.el
+++ custom/custom-init.el
@@ -2,7 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
+;;proxy for install
+(setq url-proxy-services
+      '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
+        ("http" . "192.168.2.91:1081")
+        ("https" . "192.168.2.91:1081")))
+
 ;; use-package in future
+
+
 (require 'custom-profile)
 (require 'custom-cn)
 (require 'custom-org)
