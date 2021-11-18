a9c1a3fc07bd704b58392f7f31d132dde14252d6
Leo Victor <zhangguoliang@zhaodongdb.com>
2021-11-18 10:52:51 +0800

    update

--- custom/custom-java.el
+++ custom/custom-java.el
@@ -3,17 +3,21 @@
 ;;; java coding
 ;;; Code:
 
-(when (maybe-require-package 'meghanada)
-  (progn
-    (add-hook 'java-mode-hook
-              (lambda ()
-                ;; meghanada-mode on
-                (meghanada-mode t)
-                (flycheck-mode +1)
-                (setq c-basic-offset 2)
-                ;; use code format
-                (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
-    ))
+(setenv "JAVA_HOME"  "/home/leo/.bin/.bin/jdk1.8.0_281")
+(setq lsp-java-java-path "/home/leo/.bin/.bin/jdk1.8.0_281/bin/java")
+
+
+;;      (when (maybe-require-package 'meghanada)
+;;        (progn
+;;          (add-hook 'java-mode-hook
+;;                    (lambda ()
+;;                      ;; meghanada-mode on
+;;                      (meghanada-mode t)
+;;                      (flycheck-mode +1)
+;;                      (setq c-basic-offset 2)
+;;                      ;; use code format
+;;                      (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+;;          ))
 
 
 
