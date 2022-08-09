374820f62d830d11152527b3db3f8c8143657657
zhangguoliang <zhangguoliang@zhaodongdb.com>
2022-08-09 13:02:20 +0800

    update

--- custom/custom-cn.el
+++ custom/custom-cn.el
@@ -12,5 +12,12 @@
     (setq default-input-method 'rime)
     ))
 
+
+(set-face-attribute 'default nil :font (font-spec :family "JetBrains Mono" :size 24))
+(set-fontset-font t 'unicode (font-spec :family "Noto Color Emoji" :size 24))
+(set-fontset-font t '(#x2ff0 . #x9ffc) (font-spec :family "Sarasa Mono Slab SC" :size 24 ))
+
+
+
 (provide 'custom-cn)
 ;;; custom-cn.el ends here
