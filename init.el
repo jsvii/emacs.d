c87ca086634918d09234cb7b14d5a16bade6c8be
Leo Victor <emailtoleo@qq.com>
2023-11-04 10:39:35 +0800

    update

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
 ;; a number of other files.
 
 ;;; Code:
+(setq package-check-signature nil)
 
 ;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
 ;;(setq debug-on-error t)
